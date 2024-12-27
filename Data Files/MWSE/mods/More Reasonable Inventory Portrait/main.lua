a18baea062d5561a18edf1a6c516d2e61f3087cb
Pete Goodfellow <petethegoat@gmail.com>
2024-12-27 17:50:11 +0000

    Bump version to 1.1, add vertical position option.

--- Data Files/MWSE/mods/More Reasonable Inventory Portrait/main.lua
+++ Data Files/MWSE/mods/More Reasonable Inventory Portrait/main.lua
@@ -1,40 +1,50 @@
 local defaultConfig = {
-    maxHeight = 480
+    maxHeight = 480,
+    positionVertical = 0.0,
 }
 local configPath = "More Reasonable Inventory Portrait"
-local myConfig = mwse.loadConfig(configPath, defaultConfig)
-
+local config = mwse.loadConfig(configPath, defaultConfig)
 
 --- @param e uiActivatedEventData
-local function uiActivatedCallback(e)
-    e.element:findChild("MenuInventory_character_layout").maxHeight = myConfig.maxHeight
+local function updatePortrait(e)
+    local portrait = e.element:findChild("MenuInventory_character_layout")
+    portrait.maxHeight = config.maxHeight
+    portrait.parent.childAlignY = config.positionVertical
 end
-event.register(tes3.event.uiActivated, uiActivatedCallback, { filter = "MenuInventory" })
+event.register(tes3.event.uiActivated, updatePortrait, { filter = "MenuInventory" })
 
 
 local function registerModConfig()
-    local template = mwse.mcm.createTemplate({
+    local template = mwse.mcm.createTemplate{
         name = configPath,
-        config = myConfig
-    })
+        config = config,
+    }
 
     template:register()
-    template:saveOnClose(configPath, myConfig)
-    local page = template:createPage({ label = "Settings" })
+    template:saveOnClose(configPath, config)
+    local page = template:createPage{ label = "Settings" }
 
-    page:createInfo({
+    page:createInfo{
         label = configPath,
-        text = "Version 1.0.0\nCreated by Pete Goodfellow\non 27 Dec 2024"
-    })
+        text = "Version 1.1.0\nCreated by Pete Goodfellow\non 27 Dec 2024"
+    }
 
-    page:createSlider({
+    page:createSlider{
         label = "Max Height",
         configKey = "maxHeight",
         min = 120,
         max = 800,
         step = 10,
         jump = 10,
+    }
+
+    page:createPercentageSlider{
+        label = "Vertical Position",
+        configKey = "positionVertical",
+    }
+
+    page:createInfo{
         text = "Restart the game or load a save to see the effect.",
-    })
+    }
 end
 event.register(tes3.event.modConfigReady, registerModConfig)
